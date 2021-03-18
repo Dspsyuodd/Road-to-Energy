3f78e3bca9ffa59542486703accb783c9f879360
hypevade <schkalka@gmail.com>
2021-03-19 02:57:12 +0300

    menu

---
+++ Road to energy/Rode to Energy/Assets/Scripts/Button_script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Button_script : MonoBehaviour
+{
+    public void Exit_game()
+    {
+        Application.Quit();
+    }
+
+    public void Start_game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
