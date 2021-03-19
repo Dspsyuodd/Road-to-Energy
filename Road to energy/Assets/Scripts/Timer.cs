e0631bf21f4145e75ff65987d51c57ae6b9673be
hypevade <schkalka@gmail.com>
2021-03-19 22:16:37 +0300

    123

--- Road to energy/Assets/Scripts/Timer.cs
+++ Road to energy/Assets/Scripts/Timer.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    
     public static float time_start = 60f;
     public Text TimerText;
     void Start()
@@ -16,5 +17,12 @@ public class Timer : MonoBehaviour
     {
         time_start -= Time.deltaTime;
         TimerText.text = Mathf.Round(time_start).ToString();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("UpTime"))
+        {
+            time_start += 20f;
+        }
     }
 }
