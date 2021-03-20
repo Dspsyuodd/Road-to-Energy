1ef75d2bbbe3a7c179e5954284a4bdf9771ef71a
Dspsyuodd <varlamovartem1111@gmail.com>
2021-03-20 15:19:05 +0400

    123

---
+++ Road to energy/Assets/Scripts/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    [SerializeField] private GameObject wall;
+    [SerializeField] private Sprite button2;
+    [SerializeField] private float coordY;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = button2;
+            gameObject.GetComponent<Collider2D>().enabled = false;
+
+            wall.transform.position += new Vector3(0, coordY, 0);
+        }
+    }
+}
