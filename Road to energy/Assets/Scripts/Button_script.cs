e6ec26657566a965d26df4ace0b0c3ae8e057ce7
Dspsyuodd <varlamovartem1111@gmail.com>
2021-03-20 23:26:09 +0400

    123

--- Road to energy/Assets/Scripts/Button_script.cs
+++ Road to energy/Assets/Scripts/Button_script.cs
@@ -8,19 +8,37 @@ public class Button_script : MonoBehaviour
 {
     public void Exit_game()
     {
+        Invoke("Exit", 1f);
+    }
+
+    private void Exit()
+    {
         Application.Quit();
     }
 
     public void Start_game()
+    {
+        Invoke("StartG", 1f);
+    }
+    private void StartG()
     {
         SceneManager.LoadScene(1);
     }
 
     public void Restart()
     {
+        Invoke("Rest", 1f);
+    }
+    private void Rest()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
     public void Exit_to_menu()
+    {
+        Invoke("ExitM", 1f);
+    }
+    private void ExitM()
     {
         SceneManager.LoadScene(0);
     }
